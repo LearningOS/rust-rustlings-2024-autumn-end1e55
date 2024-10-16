0e49fb2fbe55b13bc527e1b5e5380a12596c855e
end1e55 <1991243211@qq.com>
2024-10-16 20:32:48 +0800

    update

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -1,4 +1,4 @@
-// box1.rs
+ // box1.rs
 //
 // At compile time, Rust needs to know how much space a type takes up. This
 // becomes problematic for recursive types, where a value can have as part of
@@ -18,11 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +34,11 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    List::Cons(1, Box::new(List::Cons(2, Box::new(List::Nil))))
 }
 
 #[cfg(test)]
